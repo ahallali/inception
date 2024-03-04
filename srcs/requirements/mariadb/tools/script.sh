79b0d45e3881b08c6f6f0fe75120870a56e3fa04
AhmedAllali <allali156789@hotmail.com>
2024-03-04 05:15:51 +0100

    add user to wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -16,9 +16,9 @@ n
 EOF
 
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb -e "FLUSH PRIVILEGES;"
-
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER2}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD2}';"
 mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 exec mariadbd
--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -10,11 +10,11 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root --path=/var/www/wordpress
 ./wp-cli.phar config create	--allow-root --path=/var/www/wordpress \
 											--dbname=$SQL_DATABASE \
-											--dbuser=$SQL_USER \
+											--dbuser=$DB_USER \
 											--dbpass=$SQL_PASSWORD \
 											--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
-./wp-cli.phar core install --path=/var/www/wordpress --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
-# ./wp-cli.phar user create  $USER_NAME $USER_EMAIL --user_pass=$USER_PASS --allow-root
+./wp-cli.phar core install --path=/var/www/wordpress --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
+./wp-cli.phar user create --path=/var/www/wordpress $USER_NAME $USER_EMAIL --user_pass=$USER_PASS --allow-root
 service php7.4-fpm stop
 /usr/sbin/php-fpm7.4 -F	
