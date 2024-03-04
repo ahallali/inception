a8b8ae7cee6eb1c678d2aefa9a43dc1789868b18
AhmedAllali <allali156789@hotmail.com>
2024-03-04 19:44:57 +0100

    condition to download wp once

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -3,10 +3,14 @@ sleep 10
 # service start php-fpm7.4
 sed -i 's/^listen =.*/listen = wordpress:9000/' /etc/php/7.4/fpm/pool.d/www.conf
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# php wp-cli.phar --info
 chmod +x wp-cli.phar
+# php wp-cli.phar --info
 # mkdir -p /usr/local/bin/wp
 # mv wp-cli.phar /usr/local/bin/wp
+
+folder_path=/var/www/wordpress
+if [ ! -d $folder_path ];
+then 
 ./wp-cli.phar core download --allow-root --path=/var/www/wordpress
 ./wp-cli.phar config create	--allow-root --path=/var/www/wordpress \
 											--dbname=$SQL_DATABASE \
@@ -16,5 +20,6 @@ chmod +x wp-cli.phar
 
 ./wp-cli.phar core install --path=/var/www/wordpress --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
 ./wp-cli.phar user create --path=/var/www/wordpress $USER_NAME $USER_EMAIL --user_pass=$USER_PASS --allow-root
+fi
 service php7.4-fpm stop
 /usr/sbin/php-fpm7.4 -F	
